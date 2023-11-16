aca24a56b4e1a5abb9280663431455526518fce6
DanielCzerniejewicz <programowaniedaniel2007@gmail.com>
2023-11-16 09:21:23 +0100

    Poprawka

---
+++ main.js
@@ -0,0 +1,19 @@
+function czerwone()
+{
+    document.body.style.background ='#FF2222';
+}
+
+function niebieskie()
+{
+    document.body.style.background = '#0000FF';
+}
+
+function zielone()
+{
+    document.body.style.background = '#11FF00'
+}
+
+function reset_przycisk()
+{
+    document.body.style.background = '#000000'
+}
